5cc9aa8daf5a2b71f6636ed87255da0f975fb5c3
RohitM1518 <ROHIT.20211CSE0674@presidencyuniversity.in>
2024-11-20 18:54:41 +0530

    Implemented User Controllers

--- server/src/utils/asyncHandler.js
+++ server/src/utils/asyncHandler.js
@@ -1,17 +1,5 @@
-class APIError extends Error{
-    constructor(statusCode,message="Something Went Wrong",error=[],stack=""){
-        super();
-        this.statusCode = statusCode;
-        this.message = message;
-        this.error = error;
-        this.success = false;
-        if(stack){
-            this.stack=stack
-        }else{
-            Error.captureStackTrace(this,this.constructor)
-        }
-
+export const asyncHandler=(fn)=>{
+    return (req,res,next)=>{
+        Promise.resolve(fn(req,res,next)).catch(err=>next(err));
     }
 }
-
-export default APIError;
